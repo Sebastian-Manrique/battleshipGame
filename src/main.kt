666f8e92d43c98d30e92f487859caf457bd1863a
WildRepublicMini <smanriquemontiel@gmail.com>
2024-08-01 21:28:04 +0200

    Endend for today

--- src/main.kt
+++ src/main.kt
@@ -2,21 +2,23 @@ import java.awt.BorderLayout
 import java.awt.Dimension
 import javax.swing.JFrame
 import javax.swing.JScrollPane
-import javax.swing.JTextArea
+import javax.swing.JTextField
 
 fun main() {
-    val textArea = JTextArea()
-    textArea.setText("Here it goes the text")
-    val scrollPane = JScrollPane(textArea)
+
+    val frame = JFrame("Hello, Kotlin/Swing")
+    var t = JTextField()
     val list = arrayListOf<Int>() //Array
     for (i in 1..10) {
         list.addAll(listOf(i))
     }
     for (i in list.indices) { //count the array
-        print(list[i])
+        print("["+list[i]+"]")
+        t = JTextField("" + "["+list[i]+"]")
+        val scrollPane = JScrollPane(t)
+        frame.getContentPane().add(scrollPane, BorderLayout.CENTER)
     }
-    val frame = JFrame("Hello, Kotlin/Swing")
-    frame.getContentPane().add(scrollPane, BorderLayout.CENTER)
+
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE)
     frame.setSize(Dimension(600, 400))
     frame.setLocationRelativeTo(null)
