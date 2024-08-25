3f54724492224bf802055f6b6c7e45f491d639b0
WildRepublicMini <smanriquemontiel@gmail.com>
2024-08-26 00:51:02 +0200

    Adding the Grid

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,37 +1,60 @@
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
+@Composable
+@Preview
+fun App() {
+
+    MaterialTheme {
+        gridCall()
+    }
+}
 
 @Composable
-fun App() {
-    MaterialTheme {
-        buttonCall()
-    }
+fun gridCall() {
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(11),
+        content = {
+            items(11) { i ->
+                Box(
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .aspectRatio(1f)
+                        .clip(RoundedCornerShape(5.dp))
+                        .background(Color.Gray),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(text = "$i")
+                }
+            }
+        }
+    )
+
 }
 
 fun main() = application {
     Window(onCloseRequest = ::exitApplication) {
         App()
     }
-}
-
-@Composable
-fun buttonCall(){
-    Row{
-        Button(onClick = {
-        }) {
-            Text("Acabar turno")
-        }
-    }
-Spacer(modifier = Modifier.fillMaxSize().drawBehind { drawCircle(Color.Red) })
 }
