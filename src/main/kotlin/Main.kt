4e122fbaf4302b9f0652dbaa56b942f8899b5b17
WildRepublicMini <smanriquemontiel@gmail.com>
2024-08-26 02:40:09 +0200

    Ending for today

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,19 +1,16 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -25,27 +22,52 @@ import androidx.compose.ui.window.application
 @Composable
 @Preview
 fun App() {
-
     MaterialTheme {
-        gridCall()
+        Box(
+            modifier = Modifier
+                .fillMaxSize() // Take all the space available
+                .background(Color.Black) // background black
+        ) {
+            gridCall()
+        }
     }
 }
 
 @Composable
 fun gridCall() {
+    val listOfChars = listOf(' ', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J')
     LazyVerticalGrid(
         columns = GridCells.Fixed(11),
         content = {
-            items(11) { i ->
-                Box(
-                    modifier = Modifier
-                        .padding(8.dp)
-                        .aspectRatio(1f)
-                        .clip(RoundedCornerShape(5.dp))
-                        .background(Color.Gray),
-                    contentAlignment = Alignment.Center
-                ) {
-                    Text(text = "$i")
+            for (x in 0..10) {
+                for (y in 0..10) {
+                    items(1) {
+                        Box(
+                            modifier = Modifier
+                                .padding(1.dp)
+                                .aspectRatio(1f)
+                                .clip(RoundedCornerShape(5.dp))
+                                .background(
+                                    when {
+                                        x == 0 && y == 0 -> Color.Black // Paint the box in the position (0, 0) black
+                                        else -> Color.Gray
+                                    }
+                                )
+                                .then(
+                                    if (x == 0 || y == 0) {
+                                        Modifier
+                                    } else {
+                                        Modifier.clickable {
+                                            println("Touched!!")
+                                        }
+                                    }
+                                ),
+                            contentAlignment = Alignment.Center,
+                        ) {
+                            if (x == 0) Text(text = "${listOfChars[y]}")
+                            else if (y == 0) Text(text = "$x")
+                        }
+                    }
                 }
             }
         }
