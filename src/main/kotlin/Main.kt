15dc3ac6d43e77494619a5e15f3086de620fd793
WildRepublicMini <smanriquemontiel@gmail.com>
2024-09-03 01:27:04 +0200

    Changing the sizes

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,23 +1,23 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.minimumInteractiveComponentSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
 
 @Composable
 @Preview
@@ -25,6 +25,7 @@ fun App() {
     MaterialTheme {
         Box(
             modifier = Modifier
+                .defaultMinSize(minHeight = 900.dp)
                 .fillMaxSize() // Take all the space available
                 .background(Color.Black) // background black
         ) {
@@ -44,8 +45,8 @@ fun gridCall() {
                     items(1) {
                         Box(
                             modifier = Modifier
-                                .padding(1.dp)
-                                .aspectRatio(1f)
+                                .padding(0.5.dp) // Reduce padding to make items smaller
+                                .aspectRatio(0.8f) // Adjust aspect ratio to make the items smaller
                                 .clip(RoundedCornerShape(5.dp))
                                 .background(
                                     when {
@@ -76,7 +77,10 @@ fun gridCall() {
 }
 
 fun main() = application {
-    Window(onCloseRequest = ::exitApplication) {
+    Window(
+        onCloseRequest = ::exitApplication,
+        state = rememberWindowState(size = DpSize(900.dp, 925.dp))  // Specify  the inicial size of the window
+    ) {
         App()
     }
 }
